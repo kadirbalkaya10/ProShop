90493235ebcfee6d2f6e3c5fcd819d3a00a7d7da
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2024-07-23 10:52:38 -0400

    Created Header component ,using bootstrap react" ,installed bootstrap react , and react icons

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,15 @@
-import React from "react";
+import Header from "./components/Header";
+import { Container } from "react-bootstrap";
 
 export default function App() {
-  return <div>app</div>;
+  return (
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome To ProShop</h1>
+        </Container>
+      </main>
+    </>
+  );
 }
