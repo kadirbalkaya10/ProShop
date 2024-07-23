cd0909d25fd57f6f847b67ae170b7db02e979f6a
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2024-07-23 14:41:09 -0400

    Installed react router dom ,Created Routes for pages ,Changed a tags with Link from react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
+import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { Container } from "react-bootstrap";
-import HomeScreen from "./screens/HomeScreen";
 
 export default function App() {
   return (
@@ -9,7 +9,7 @@ export default function App() {
       <Header />
       <main className='py-3'>
         <Container>
-          <HomeScreen />
+          <Outlet />
         </Container>
       </main>
       <Footer />
