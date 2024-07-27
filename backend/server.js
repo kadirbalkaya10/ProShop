e4cf99ddbd7fe9cb14770fd0e25745169f3a4bb9
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2024-07-27 12:20:28 -0400

    Fetching data, from products fodler, settin routes for , api/product & api/product/:id to get info

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import products from "./data/products.js";
 import express from "express";
 
 const port = 3000;
@@ -6,6 +7,15 @@ const app = express();
 
 app.get("/", (req, res) => {
   res.send("API is running");
+});
+
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/api/product/:id", (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
 });
 
 app.listen(port, () =>
