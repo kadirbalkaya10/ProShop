01b728146513fad95631b1687c6b7b433471fc0e
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2024-07-27 13:40:47 -0400

    Added comments for server.js to explain each line of code

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,30 @@
 import products from "./data/products.js";
+// Import the 'express' module to create the server
 import express from "express";
 
+// / Define the port number the server will listen on
 const port = 3000;
 
+// Initialize the Express application
 const app = express();
 
+// Setting up a route for the home page respond with a simple message
 app.get("/", (req, res) => {
   res.send("API is running");
 });
 
+// Setting up a route to get all products
 app.get("/api/products", (req, res) => {
-  res.json(products);
+  res.json(products); // Sending the products data as a JSON response
 });
 
+// Setting up a route to get a single product by its ID
 app.get("/api/product/:id", (req, res) => {
   const product = products.find((p) => p._id === req.params.id);
   res.json(product);
 });
 
+// Starting the server and having it listen on the specified port
 app.listen(port, () =>
   console.log(`Server running on PORT http://localhost:${port}`)
 );
