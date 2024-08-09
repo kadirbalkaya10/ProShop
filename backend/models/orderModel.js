a83ae1f3f8fae5de89d8b0d44166aa2ac9ca1773
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2024-08-09 10:55:05 -0400

    Created order Model, created orderScheme

---
+++ backend/models/orderModel.js
@@ -0,0 +1,84 @@
+import mongoose from "mongoose";
+
+const orderScheme = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Type.ObjectId,
+      required: true,
+      ref: "User",
+    },
+
+    orderItems: [
+      {
+        name: { type: String, required: true },
+        quantity: { type: Number, required: true },
+        image: { type: String, required: true },
+        price: { type: Number, required: true },
+        product: {
+          type: mongoose.Schema.Type.ObjectId,
+          required: true,
+          ref: "Product",
+        },
+      },
+    ],
+    shippingAddress: {
+      address: { type: String, required: true },
+      city: { type: String, required: true },
+      postalCode: { type: String, required: true },
+      country: { type: String, required: true },
+    },
+    paymentMethod: {
+      type: String,
+      required: true,
+    },
+    paymentResult: {
+      id: { type: String },
+      status: { type: String },
+      updatedTime: { type: String },
+      email_address: { type: String },
+    },
+    itemsPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+    taxPrice: {
+      type: number,
+      required: true,
+      default: 0.0,
+    },
+    shippingPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+    totalPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+    isPaid: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    paidAt: {
+      type: Date,
+    },
+    isDelivered: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    deliveredAt: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Order = mongoose.Model("Order", orderScheme);
+
+export default Order;
