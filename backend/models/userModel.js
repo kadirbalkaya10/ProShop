44e6bc2a505ea88beedf05052eada65e1e9d2c52
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2024-08-09 10:39:35 -0400

    Created User Model

---
+++ backend/models/userModel.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const userScheme = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.Model("User", userScheme);
+
+export default User;
