96cb7541a131daceef5eccbb74b38515e7950892
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2024-08-13 11:13:01 -0400

    Created productController , Organized code , making api request from controller

---
+++ backend/controllers/productController.js
@@ -0,0 +1,25 @@
+import asyncHandler from "../middleware/asyncHandler.js";
+import Product from "../models/productModel.js";
+
+// @desc Fetch all products
+// @route /api/products
+// @access Public
+const getProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({});
+  res.json(products);
+});
+
+// @desc Fetch a  product
+// @route /api/products/:id
+// @access Public
+const getProductByID = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    return res.json(product);
+  } else {
+    res.status(404);
+    throw new Error("Resource not found!");
+  }
+});
+
+export { getProductByID, getProducts };
