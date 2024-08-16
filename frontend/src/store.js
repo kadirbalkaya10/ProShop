b40ae90bcfb04f57a6dfdcca59189e09041770ec
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2024-08-16 14:01:57 -0400

    Created store, for redux

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { apiSlice } from "./slices/appSlice";
+import { apiSlice } from "./slices/apiSlice.js";
 
 const store = configureStore({
   reducer: { [apiSlice.reducerPath]: apiSlice.reducer },
