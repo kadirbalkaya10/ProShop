00b288e0f1b806357e4643c340f8abc380be7d31
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2024-08-30 13:05:11 -0400

    Bug fixed, he issue causing itemsPrice, taxPrice, and totalPrice to display as "NaN" in Redux DevTools likely stems from incorrect handling of number types during your calculations

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -5,7 +5,7 @@ const initialState = localStorage.getItem("cart")
   : { cartItems: [] };
 
 const addDecimals = (num) => {
-  return Math.round((num * 100) / 100).toFixed(2);
+  return (Math.round(num * 100) / 100).toFixed(2);
 };
 
 const cartSlice = createSlice({
@@ -14,6 +14,8 @@ const cartSlice = createSlice({
   reducers: {
     addToCart: (state, action) => {
       const item = action.payload;
+      item.price = Number(item.price);
+      item.qty = Number(item.quantity);
 
       const existItem = state.cartItems.find((x) => x._id === item._id);
 
@@ -26,20 +28,21 @@ const cartSlice = createSlice({
       }
 
       //Calculate items price
-      state.itemsPrice = addDecimals(
-        state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
+      const itemsPrice = state.cartItems.reduce(
+        (acc, item) => acc + (item.price * 100 * item.qty) / 100,
+        0
       );
+      state.itemsPrice = addDecimals(itemsPrice);
+
       //Calculate shipping price(IF order is over 100$ then free,else 10$ shipping price)
-      state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
+      const shippingPrice = itemsPrice > 100 ? 0 : 10;
+      state.shippingPrice = addDecimals(shippingPrice);
       //Calculate tax price(15%tax )
-      state.taxPrice = addDecimals(Number(0.15 * state.itemsPrice).toFixed(2));
+      const taxPrice = 0.15 * itemsPrice;
+      state.taxPrice = addDecimals(taxPrice);
       //Calculate total price
-      state.totalPrice = (
-        Number(state.itemsPrice) +
-        Number(state.shippingPrice) +
-        Number(state.taxPrice)
-      ).toFixed(2);
-      localStorage.setItem("cart", JSON.stringify(state));
+      const totalPrice = itemsPrice + shippingPrice + taxPrice;
+      state.totalPrice = addDecimals(totalPrice);
     },
   },
 });
