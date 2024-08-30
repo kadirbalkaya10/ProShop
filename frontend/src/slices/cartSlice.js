8058a6c86f8ac2298c5fbc3318d95512a008a435
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2024-08-30 14:25:18 -0400

    Implementing cartUtil , helper function

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -1,12 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { updateCart } from "../utils/cartUtils";
 
 const initialState = localStorage.getItem("cart")
   ? JSON.parse(localStorage.getItem("cart"))
   : { cartItems: [] };
-
-const addDecimals = (num) => {
-  return (Math.round(num * 100) / 100).toFixed(2);
-};
 
 const cartSlice = createSlice({
   name: "cart",
@@ -27,22 +24,7 @@ const cartSlice = createSlice({
         state.cartItems = [...state.cartItems, item];
       }
 
-      //Calculate items price
-      const itemsPrice = state.cartItems.reduce(
-        (acc, item) => acc + (item.price * 100 * item.qty) / 100,
-        0
-      );
-      state.itemsPrice = addDecimals(itemsPrice);
-
-      //Calculate shipping price(IF order is over 100$ then free,else 10$ shipping price)
-      const shippingPrice = itemsPrice > 100 ? 0 : 10;
-      state.shippingPrice = addDecimals(shippingPrice);
-      //Calculate tax price(15%tax )
-      const taxPrice = 0.15 * itemsPrice;
-      state.taxPrice = addDecimals(taxPrice);
-      //Calculate total price
-      const totalPrice = itemsPrice + shippingPrice + taxPrice;
-      state.totalPrice = addDecimals(totalPrice);
+      return updateCart(state);
     },
   },
 });
