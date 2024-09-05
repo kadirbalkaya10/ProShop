269ff4be639d05053e1240c10112d9cc971f25f6
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2024-09-05 13:10:05 -0400

    Added middleware to receive json data from http request , and urlencoded

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,11 @@ connectDB();
 // Initialize the Express application
 const app = express();
 
+//  Body parser middleware
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // Setting up a route for the home page respond with a simple message
 app.get("/", (req, res) => {
   res.send("API is running");
