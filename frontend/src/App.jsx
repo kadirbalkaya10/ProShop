049412a23f03166f488617a72d5c567ff7dd260f
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2024-09-06 15:07:15 -0400

    Implementing toastContainer from react-toastify

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,6 @@
 import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { Container } from "react-bootstrap";
@@ -13,6 +15,7 @@ export default function App() {
         </Container>
       </main>
       <Footer />
+      <ToastContainer />
     </>
   );
 }
