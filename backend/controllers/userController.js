bdc4688d1adcc242fb81036b78a89ce2d73f0c3b
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2024-09-06 09:11:40 -0400

    Update user profile function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -98,7 +98,28 @@ const getUserProfile = asyncHandler(async (req, res) => {
 // @access Private
 
 const updateUserProfile = asyncHandler(async (req, res) => {
-  res.send("Update user profile");
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+
+    res.status(200).json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(400);
+    throw new Error("User not found");
+  }
 });
 
 // @desc Get Users
