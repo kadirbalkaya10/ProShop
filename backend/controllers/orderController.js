ad848e5029dce3f7c5cd9da93b7fd956b25f2f45
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2024-09-10 12:03:36 -0400

    Defined getOrderById function

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -55,7 +55,17 @@ const getMyOrders = asyncHandler(async (req, res) => {
 // @access Private
 
 const getOrderById = asyncHandler(async (req, res) => {
-  res.send("Get Order ById");
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+
+  if (order) {
+    res.status(200).json(order);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
 });
 
 // @desc Update order to paid
