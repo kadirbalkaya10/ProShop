b867acb06c9468dd405a5b1d53cad452a8156996
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2024-09-11 21:24:59 -0400

    Implementing paypal provider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,6 +9,7 @@ import {
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import store from "./store.js";
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 // import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/styles/bootstrap.custom.css";
 import "./assets/styles/index.css";
@@ -45,7 +46,9 @@ const router = createBrowserRouter(
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <PayPalScriptProvider deferLoading={true}>
+        <RouterProvider router={router} />
+      </PayPalScriptProvider>
     </Provider>
   </React.StrictMode>
 );
