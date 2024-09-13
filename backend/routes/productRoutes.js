8efca55e62dc5973476e1e75ca479b500537f03c
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2024-09-13 09:53:28 -0400

    Created a new route for , create a new product with post request

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,11 +1,13 @@
 import express from "express";
 const router = express.Router();
 import {
+  createProduct,
   getProductByID,
   getProducts,
 } from "../controllers/productController.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
 
-router.route("/").get(getProducts);
+router.route("/").get(getProducts).post(protect, admin, createProduct);
 router.route("/:id").get(getProductByID);
 
 export default router;
