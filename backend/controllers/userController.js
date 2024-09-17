3a106499e0c34f1890e8b905b8dc75daa12aed11
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2024-09-17 09:43:46 -0400

    Defined updateUser controller function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -171,7 +171,25 @@ const deleteUser = asyncHandler(async (req, res) => {
 // @access Private/admin
 
 const updateUser = asyncHandler(async (req, res) => {
-  res.send("Update user!!!! Admin Only");
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    user.isAdmin = Boolean(req.body.isAdmin);
+
+    const updatedUser = await user.save();
+
+    res.status(200).json({
+      _id: updateUser._id,
+      name: updateUser.name,
+      email: updateUser.email,
+      isAdmin: updateUser.isAdmin,
+    });
+  }else {
+    res.status(404)
+    throw new Error('User not found')
+  }
 });
 
 export {
