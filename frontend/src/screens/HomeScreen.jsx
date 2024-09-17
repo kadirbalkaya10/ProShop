547983b677b721ac67f649d66725705a5c5e745b
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2024-09-17 14:12:23 -0400

    Refactor code for paginate data , to get products with limitation only show xNumber of products

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,12 +1,14 @@
 import { Row, Col } from "react-bootstrap";
 // import products from "../products";
 import Product from "../components/Product";
+import { useParams } from "react-router-dom";
 import { useGetProductsQuery } from "../slices/productApiSlice";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
 
 function HomeScreen() {
-  const { data: products, isLoading, error } = useGetProductsQuery();
+  const { pageNumber } = useParams();
+  const { data, isLoading, error } = useGetProductsQuery({ pageNumber });
 
   return (
     <>
@@ -18,7 +20,7 @@ function HomeScreen() {
         <>
           <h1>Latest Products</h1>
           <Row>
-            {products.map((product) => (
+            {data.products.map((product) => (
               <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
                 <Product product={product} />
               </Col>
