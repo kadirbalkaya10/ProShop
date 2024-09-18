87ffe5b224cb2589da23dd4e80674c3f3255ddfe
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2024-09-18 10:59:54 -0400

    created resetCart reducer function to clear cart on localstorage when user is logout

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -43,6 +43,9 @@ const cartSlice = createSlice({
       state.cartItems = [];
       return updateCart(state);
     },
+    resetCart: (state) => {
+      state = initialState;
+    },
   },
 });
 
@@ -52,6 +55,7 @@ export const {
   saveShippingAddress,
   savePaymentMethod,
   clearCartItems,
+  resetCart,
 } = cartSlice.actions;
 
 export default cartSlice.reducer;
