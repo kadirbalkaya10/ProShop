c2f5de3b7f69216bbf3a07446886dbcff65a879d
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2025-03-24 15:22:44 -0400

    Fixed Double route

--- backend/server.js
+++ backend/server.js
@@ -29,10 +29,10 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 
-// Setting up a route for the home page respond with a simple message
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
+// // Setting up a route for the home page respond with a simple message
+// app.get("/", (req, res) => {
+//   res.send("API is running");
+// });
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
